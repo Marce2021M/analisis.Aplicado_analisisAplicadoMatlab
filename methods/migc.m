7362dc44cf31903d9d7dacff32a340c69a5aea8a
marcelinoMSR <marcelino-sanchez2011@hotmail.com>
2023-03-21 02:38:36 -0600

    modificando a archivos legibles2

--- methods/migc.m
+++ methods/migc.m
@@ -1,19 +1,24 @@
 function [x,k] = migc(A,b)
 
-n = length(b);
-x = zeros(n,1);
-r = A*x - b;
-p = -r;
-kmax = n*n;
-k = 0;
+% This is a function that implements the method of conjugate gradients
+% to solve a linear system of the form Ax = b, where A is a positive
+% definite symmetric matrix.
 
-while ( norm(r)> 1.e-08 && k < kmax)
-   alpha = -(r'*p)/(p'*A*p);
-   x = x + alpha * p;
-   r = A*x - b;
-   betha = (r'*A*p)/(p'*A*p);
-   p = -r + betha*p;
-   k = k + 1;
+n = length(b); % dimension of system
+x = zeros(n,1); % initialize solution to all zeros
+r = A*x - b; % compute initial residual
+p = -r; % set initial search direction to negative residual
+kmax = n*n; % maximum number of iterations
+k = 0; % initialize iteration counter
+
+while (norm(r) > 1.e-08 && k < kmax) % loop until convergence or max iterations
+   alpha = -(r'*p)/(p'*A*p); % compute step size
+   x = x + alpha * p; % update solution
+   r = A*x - b; % compute new residual
+   betha = (r'*A*p)/(p'*A*p); % compute correction factor for search direction
+   p = -r + betha*p; % update search direction
+   k = k + 1; % increment iteration counter
 end
 
+% The function returns the solution x and the number of iterations k.
 end
